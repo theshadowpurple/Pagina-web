1fbffea0734f8319f3c0b1c9fd909a8ae7dfd270
theshadowpurple <dariorafaelmartinez8@gmail.com>
2026-06-11 03:16:56 -0600

    version 2

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ const boton = document.getElementById('elBoton');
 
 // Escuchamos cuando el usuario hace clic
 boton.addEventListener('click', () => {
-    alert('Te invito a tomar una tapioca el dia sabado, aceptas amor?');
+    // confirm() abre la ventana con dos opciones (Aceptar / Cancelar)
+    const acepto = confirm('Te invito a tomar una tapioca el dia sabado, aceptas amor?');
+    
+    // Si le da clic a "Aceptar", la variable 'acepto' será true
+    if (acepto) {
+        alert('¡Siii! Sabía que aceptarías. 🧋✨ Nos vemos el sábado, te amo.');
+    } else {
+        // Si le da clic a "Cancelar" (No acepto)
+        alert('¿Cómo que no? 😱 Jajaja, ¡inténtalo de nuevo! ❤️');
+    }
 });
