5db41c9b5d61dee9b3c417403f382d4ec3bc59d6
theshadowpurple <dariorafaelmartinez8@gmail.com>
2026-06-11 01:12:37 -0600

    actu

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ const boton = document.getElementById('elBoton');
 // Escuchamos cuando el usuario hace clic
 boton.addEventListener('click', () => {
     alert('Te invito a tomar unas tapiocas el dia sabado, aceptas amor?');
+    cancelbutton = confirm('¿Quieres aceptar la invitación?');
+    if (cancelbutton) {
+        alert('¡Genial! Nos vemos el sábado para disfrutar de unas deliciosas tapiocas juntos. ¡Estoy emocionado/a por nuestra cita!');
+    } else {
+        alert('Entiendo, no hay problema. Si cambias de opinión, estaré aquí para disfrutar de unas tapiocas contigo en otro momento. ¡Cuídate!');
+    }
 })
