6f47b6d53325c19cc676cfecb1852285bbda1a5d
theshadowpurple <dariorafaelmartinez8@gmail.com>
2026-06-11 01:06:29 -0600

    actualizacion

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const boton = document.getElementById('elBoton');
 
 // Escuchamos cuando el usuario hace clic
 boton.addEventListener('click', () => {
-    alert('Te invito a tomar una tapioca el dia sabado, aceptas amor?');
+    alert('Te invito a tomar unas tapiocas el dia sabado, aceptas amor?');
 })
