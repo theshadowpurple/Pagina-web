f1c258d712d29effedb9d7e869f8ca40747092b2
theshadowpurple <dariorafaelmartinez8@gmail.com>
2026-06-11 01:43:36 -0600

    a

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const boton = document.getElementById('elBoton');
 dialog.addEventListener('click', function() {
     // Mostramos un mensaje de alerta
     alert('quieres salir a tomar una tapioca conmigo?');
-});
+})
