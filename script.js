52d2c503de16e3d3073e708ba3b0910492a213d0
theshadowpurple <dariorafaelmartinez8@gmail.com>
2026-06-11 00:43:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleccionamos el botón por su ID
+const boton = document.getElementById('elBoton');
+
+// Escuchamos cuando el usuario hace clic
+boton.addEventListener('click', () => {
+    alert('¡Hola! Has activado la programación en JavaScript de esta página. 🚀');
+});
