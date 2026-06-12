65e2ebcaffb991f6cda1c34670e5aab821b58893
theshadowpurple <dariorafaelmartinez8@gmail.com>
2026-06-11 18:13:18 -0600

    actu grande1

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Seleccionamos el botón por su ID
 const boton = document.getElementById('elBoton');
+const musica = new Audio('./musica/amor-completo.mp3');
+musica.loop = true; // Reproducir en bucle
 
+window.onload = () => {
+    musica.play().catch(error => {
+        console.log("Autoplay bloqueado por el navegador. Esperando interacción del usuario.");
+    });
+};
 // Escuchamos cuando el usuario hace clic
 boton.addEventListener('click', () => {
     // confirm() abre la ventana con dos opciones (Aceptar / Cancelar)
@@ -10,7 +17,11 @@ boton.addEventListener('click', () => {
     if (acepto) {
         alert('¡Siii! Sabía que aceptarías. 🧋✨ Nos vemos el sábado, te amo.');
     } else {
+        musica.pause();
         // Si le da clic a "Cancelar" (No acepto)
         alert('¿Cómo que no? 😱 Jajaja, ¡inténtalo de nuevo! ❤️');
+        
+        window.close(); // Cierra la ventana del navegador
     }
+
 });
